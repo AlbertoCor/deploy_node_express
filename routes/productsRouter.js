feb65db52938b792fb61820def66474ecc0a2cb2
Alberto Coronado <ac.insite15@live.com>
2021-10-27 15:34:55 -0500

    Add services structure on its files, to separate from route files, and add try catch as error capture, only on products, lack catgories and users data

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -5,45 +5,44 @@ const ProductsService = require ('./../services/productService')
 const router = express.Router();
 const service = new ProductsService();
 
-router.get('/', (req, res) =>{     // New route
-    const products = service.find();
+router.get('/', async (req, res) =>{     // New route
+    const products = await service.find();
     res.json(products);
   });
 
   router.get('/filter', (req, res) => {
     res.send('im a filter')
   });
-  
+
   router.get('/:id', (req, res) => {
     const {id} = req.params; // ecma6 using: const { object to search in params }, no ecma (req.params.id)
     const product = service.findone(id);
     res.json(product);
   });
 
-router.post('/', (req, res) => { // collect post data in json from postman or insomnia
+router.post('/', async (req, res) => { // collect post data in json from postman or insomnia
   const body = req.body;
-  res.status(201).json({
-    message: 'created', 
-    data: body
-  })
-})
-
-router.patch('/:id', (req, res) => { // collect post data in json from postman or insomnia
-  const { id } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'update', 
-    data: body,
-    id,
-  })
-})
-
-router.delete('/:id', (req, res) => { // delete data in json from postman or insomnia
-  const { id } = req.params;
-  res.json({
-    message: 'deleted', 
-    id,
-  });
+  const newProduct = await service.create(body)
+  res.status(201).json(newProduct);
 });
-  
+
+router.patch('/:id', async (req, res) => { // collect post data in json from postman or insomnia
+  try{
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
+});
+
+router.delete('/:id', async (req, res) => { // delete data in json from postman or insomnia
+  const { id } = req.params;
+  const del = await service.delete(id);
+  res.json(del);
+});
+
 module.exports = router;
