3a11ae134c84a52d56723052d247579fe1bbf7e3
Alberto Coronado <ac.insite15@live.com>
2021-11-03 17:08:53 -0600

    Added deplyment setup with heoku nad a html file for test permits for request, deploy

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const cors = require('cors')
 const routerApi = require('./routes')
 
 const {logErrors, errorHandler,boomerrorHandler  } = require('./middlewares/errorHandler')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json()); // recive info from post data
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co']; // list of locations to permit request
+const options = { // callback function to validate origin to connect to api
+  origin: (origin, callback) => {
+    if ( whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback( new Error('no permit'));
+    }
+  }
+}
+app.use(cors());
 
 app.get('/', (req, res) =>{     // Default route
   res.send("hello 1st server on express");
